857b7bbec4ba1153d819d5c4466b8c956bb35a7e
PrabhaSDET-TestV <prabhakaranr@testvagrant.com>
2025-03-17 11:51:44 +0530

    Update

---
+++ pages/ThemeBuilder.ts
@@ -0,0 +1,31 @@
+import { Page, Locator } from '@playwright/test';
+
+export class ThemeBuilderPage {
+  readonly page: Page;
+  readonly allRows: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+
+    // Define all locators in the constructor like JavaScript example
+    this.allRows = page.locator('div[role="row"]');
+  }
+
+  /**
+   * Get total number of rows in the table (count of <div role="row">)
+   * @returns Promise<number>
+   */
+  async getNumberOfRows(): Promise<number> {
+    return await this.allRows.count();
+  }
+
+  /**
+   * Alias for getNumberOfRows() - get row count in table
+   * @returns Promise<number>
+   */
+  async getRowCount(): Promise<number> {
+    return await this.getNumberOfRows(); // Simple alias
+  }
+}
+
+export default ThemeBuilderPage;
